9dc96b7b9502de1925c5eaeaa6298fc90c04eed7
Ben Pease <bpease@orrcorp.com>
2020-09-14 15:45:37 -0400

    #34 Change default view of customers to List View

---
+++ src/custom-addons/orr_sale/models/res_partner.py
@@ -0,0 +1,23 @@
+from odoo import api,fields, models
+
+print("-------------------------------------testing3")
+class Partners(models.Model):
+    _inherit = "res.partner"
+
+    print("----------------------------------testing2")
+    def name_get(self):
+        orig_res = super().name_get()
+        # orig_names = {rec_id: rec_name for rec_id, rec_name in orig_res}
+        orig_names = dict(orig_res)  # even simpler
+        res = []
+        print("--------------------------------------testing")
+        for record in self:
+            # naive, ignores super():
+            name = record.name
+            # optimized, uses super():
+            name = orig_names[record.id]
+            city = record.city
+            state = record.state_id.name
+            display_name = "%s (%s, %s)" % (name, city, state)
+            res.append((record.id, display_name))
+        return res
