9f03250b77767f2f82a8a10ca2ff00b8594b61c8
troublero <tgeorge@orrcorp.com>
2020-09-16 11:47:25 -0400

    update to fix issues

--- src/custom-addons/orr_document_extend/__manifiest__.py
+++ src/custom-addons/orr_document_extend/__manifiest__.py
@@ -1,12 +1,14 @@
 {
-    'name':'ORR Sale',
+    'name':'Orr Document Exend',
+    'description':'To assign customer to view'
     'author': 'ORR Dev trg',
     'depends':[
-        'document'
+     'base',
     ],
     'application':False,
     'installable':True,
     'data': [
         
-    ]   
+    ],
+       
 } 
