36aee12ae92d25258b0159ab64bf172346772bac
troublero <tgeorge@orrcorp.com>
2020-09-23 13:52:20 -0400

    small update for store clarity

--- src/custom-addons/orr_material_purchase_requisitions/model/purchase_requisition.py
+++ src/custom-addons/orr_material_purchase_requisitions/model/purchase_requisition.py
@@ -4,4 +4,4 @@ class PurchaseRequisition(models.Model):
     _inherit = 'material.purchase.requisition'
 
     partner_id = fields.Many2one('res.partner', string='Customer',
-        related="project_id.partner_id",store=True)
+        related="project_id.partner_id", store=True)
