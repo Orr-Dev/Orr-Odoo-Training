5ff58a270f2857662b9b178f1b49b55b3a24ace5
troublero <tgeorge@orrcorp.com>
2020-10-01 14:06:39 -0400

    Add inspection notes osi-14

---
+++ src/custom-addons/orr_fieldservice/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'ORR Field Service',
+    'author': 'ORR Dev trg',
+    'depends':[
+        'fieldservice',
+    ],
+    'application':False,
+    'installable':True,
+    'data': [
+       'views/fsm_order.xml'
+    ]   
+} 
